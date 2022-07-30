39900324b55368cd59d1bdf1f573b3ddddfa8c6a
loveplay1983 <michaelxuan@hotmail.com>
2022-07-30 16:41:06 +0800

    working on templates

---
+++ study/02_template/test.py
@@ -0,0 +1,43 @@
+# _*_ coding: utf-8 _*_
+
+"""
+ author: Xiong Min Xuan
+ study, study, and study!!!
+"""
+
+import os
+from flask import Flask\
+    , render_template\
+    , flash, redirect\
+    , url_for, Markup
+
+app = Flask(__name__)
+# app.secret_key =  os.getenv("SECRET_KEY", "secret string")
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+user = {
+    'username': 'Grey Li',
+    'bio': 'A boy who loves movies and music.',
+}
+
+movies = [
+    {'name': 'My Neighbor Totoro', 'year': '1988'},
+    {'name': 'Three Colours trilogy', 'year': '1993'},
+    {'name': 'Forrest Gump', 'year': '1994'},
+    {'name': 'Perfect Blue', 'year': '1997'},
+    {'name': 'The Matrix', 'year': '1999'},
+    {'name': 'Memento', 'year': '2000'},
+    {'name': 'The Bucket list', 'year': '2007'},
+    {'name': 'Black Swan', 'year': '2010'},
+    {'name': 'Gone Girl', 'year': '2014'},
+    {'name': 'CoCo', 'year': '2017'},
+]
+
+@app.route("/watch")
+def watchlist():
+    return render_template('watchlist.html', user=user, movies=movies)
+
+@app.route("/")
+def index():
+    return "jinja2 test"
