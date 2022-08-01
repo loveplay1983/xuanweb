92a329cfa75d2fc2e1a99dc19359fa02947feef5
loveplay1983 <michaelxuan@hotmail.com>
2022-08-01 16:48:29 +0800

    office

--- study/02_template/test.py
+++ study/02_template/test.py
@@ -6,13 +6,14 @@
 """
 
 import os
-from flask import Flask\
-    , render_template\
-    , flash, redirect\
-    , url_for, Markup
+from flask import Flask \
+    , render_template \
+    , flash, redirect \
+    , url_for, Markup \
+    , flash
 
 app = Flask(__name__)
-# app.secret_key =  os.getenv("SECRET_KEY", "secret string")
+app.secret_key =  os.getenv("SECRET_KEY", "secret string")
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
@@ -34,11 +35,28 @@ movies = [
     {'name': 'CoCo', 'year': '2017'},
 ]
 
+
 @app.route("/watch")
 def watchlist():
-    return render_template('watchlist.html', user=user, movies=movies)
+    return render_template('watchlistStatic.html', user=user, movies=movies)
+
 
 @app.route("/")
 def index():
     # return "jinja2 test"
     return render_template("index.html")
+
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+@app.route("/flash")
+def showFlash():
+    flash(u"Hello, ths is flash!")
+    # return redirect(url_for("index"))
+    return "A"
+
+@app.errorhandler(404)
+def pageNotFound(e):
+    return render_template("errors/404.html"), 404
