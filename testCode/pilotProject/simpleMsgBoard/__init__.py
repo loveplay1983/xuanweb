1d655ffe1cfcee8758de1431be4fecbfca620a40
loveplay1983 <michaelxuan@hotmail.com>
2022-08-17 05:18:53 +0800

    home

--- testCode/pilotProject/simpleMsgBoard/__init__.py
+++ testCode/pilotProject/simpleMsgBoard/__init__.py
@@ -3,4 +3,15 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask["simpleMsgBoard"]
+app.config.from_pyfile("settings.py")
+app.jinja_env.trim_blocks=True
+app.jinja_env.lstrip_blocks=True
+
+db = SQLAlchemy(app)
+bootstrap=Bootstrap(app)
+moment = Moment(app)
+
+from simpleMsgBoard import views, erros, commands
+
 
---
+++ testCode/pilotProject/simpleMsgBoard/settings.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+from simpleMsgBoard import app
+
+WIN = sys.platform.startswith("win")
+if WIN:
+    prefix=
+
