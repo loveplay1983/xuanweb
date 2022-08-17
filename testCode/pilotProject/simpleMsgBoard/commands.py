4053877c920bfa853d88774381a09e9d480f0893
loveplay1983 <michaelxuan@hotmail.com>
2022-08-17 22:01:34 +0800

    home edited.

--- testCode/pilotProject/simpleMsgBoard/commands.py
+++ testCode/pilotProject/simpleMsgBoard/commands.py
@@ -4,3 +4,41 @@ Author : Michael Xuan
 Project: Study
 Email  : michaelxuan@hotmail.com
 """
+import click
+
+from simpleMsgBoard import app, db
+from simpleMsgBoard.models import Message
+from faker import Faker
+
+
+@app.cli.command()
+@click.option("--drop", is_flag=True, help="Create after drop.")
+def initdb(drop):
+    if drop:
+        click.confirm("This operation will delete the database, \
+        do you want to continue?", abort=True)
+        db.drop_all()
+        click.echo("Drop tables.")
+    db.create_all()
+    click.echo("Database is initialized.")
+
+
+@app.cli.command()
+@click.option("--count", default=20, help="Quantity of messages, default 20")
+def forge(count):
+    db.drop_all()
+    db.create_all()
+
+    fake = Faker()
+    click.echo("Working...")
+
+    for each in range(count):
+        message = Message(
+            name=fake.name(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year()
+        )
+        db.session.add(message)
+
+    db.session.commit()
+    click.echo(f"Created {count} fake messages.")
--- testCode/pilotProject/simpleMsgBoard/erros.py
+++ testCode/pilotProject/simpleMsgBoard/erros.py
@@ -4,3 +4,15 @@ Author : Michael Xuan
 Project: Study
 Email  : michaelxuan@hotmail.com
 """
+from flask import render_template
+from simpleMsgBoard import app
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("errors/404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("erros/500.html"), 500
