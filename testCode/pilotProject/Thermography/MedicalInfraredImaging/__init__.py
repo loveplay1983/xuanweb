a85f40564ca1ce64c92bed9909fb6d84317a2e2f
loveplay1983 <michaelxuan@hotmail.com>
2022-08-25 20:25:23 +0800

    strating thermography

--- testCode/pilotProject/Thermography/MedicalInfraredImaging/__init__.py
+++ testCode/pilotProject/Thermography/MedicalInfraredImaging/__init__.py
@@ -5,4 +5,16 @@ Project: Study
 Email  : michaelxuan@hotmail.com
 """
 
-# to-do
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask("MedicalInfraredImaging")
+app.config.from_pyfile("settings.py")
+app.jinja_env.trim_blocks=True
+app.jinja_env.lstrip_blocks=True
+
+db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
+
+from MedicalInfraredImaging import commands, errors, forms, models, views, settings
