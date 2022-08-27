87b47cbc67f8ffa9f5d3e4d3a02c51a3c9fdedd6
loveplay1983 <michaelxuan@hotmail.com>
2022-08-27 14:46:35 +0800

    office datetime

--- testCode/pilotProject/Thermography/MedicalInfraredImaging/static/js/script.js
+++ testCode/pilotProject/Thermography/MedicalInfraredImaging/static/js/script.js
@@ -6,3 +6,19 @@ $(function () {
         {title: render_time}
     );
 });
+
+
+// Function to format 1 in 01
+const zeroFill = n => {
+    return ('0' + n).slice(-2);
+}
+
+// Creates interval
+const interval = setInterval(() => {
+    const now = new Date();
+    const day = now.toLocaleDateString('zh-CN', {weekday: "short", year: "numeric"})
+    document.getElementById('datetime').innerHTML = day + " " +
+        zeroFill(now.getHours()) + ':' +
+        zeroFill(now.getMinutes()) + ':' +
+        zeroFill(now.getSeconds());
+}, 1000);
--- testCode/test/web/js/datatime.js
+++ testCode/test/web/js/datatime.js
@@ -6,10 +6,36 @@ const zeroFill = n => {
 // Creates interval
 const interval = setInterval(() => {
     const now = new Date();
-    const day = new Date().toLocaleDateString('zh-CN', {weekday: "short", year: "numeric"})
+    const day = now.toLocaleDateString('zh-CN', {weekday: "short", year: "numeric"})
 
-    // document.getElementById('a').innerHTML = now.getDay();
-    document.getElementById('a').innerHTML = day + " " + zeroFill(now.getHours()) + ':' +
-        zeroFill(now.getMinutes()) + ':' + zeroFill(now.getSeconds());
-    // new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"})
+    // document.getElementById('datetime').innerHTML = day
+    document.getElementById('datetime').innerHTML = day + " " +
+        zeroFill(now.getHours()) + ':' +
+        zeroFill(now.getMinutes()) + ':' +
+        zeroFill(now.getSeconds());
+    // new Date().toLocaleDateString('en-us', { weekday:"long",
+    //     year:"numeric",
+    //     month:"short",
+    //     day:"numeric"})
 }, 1000);
+
+
+// // Function to format 1 in 01
+// const zeroFill = n => {
+//     return ('0' + n).slice(-2);
+// }
+//
+// // Creates interval
+// const interval = setInterval(() => {
+//     // Get current time
+//     const now = new Date();
+//
+//     // Format date as in mm/dd/aaaa hh:ii:ss
+//     const dateTime = zeroFill((now.getMonth() + 1)) + '/' +
+//         zeroFill(now.getUTCDate()) + '/' + now.getFullYear() +
+//         ' ' + zeroFill(now.getHours()) + ':' + zeroFill(now.getMinutes()) +
+//         ':' + zeroFill(now.getSeconds());
+//
+//     // Display the date and time on the screen using div#date-time
+//     document.getElementById('date-time').innerHTML = dateTime;
+// }, 1000);
