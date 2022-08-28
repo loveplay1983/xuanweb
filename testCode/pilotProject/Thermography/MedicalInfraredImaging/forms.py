f0e16f86802819d76432c495ff7347178516d25c
loveplay1983 <michaelxuan@hotmail.com>
2022-08-28 22:29:24 +0800

    creating forms and views

--- testCode/pilotProject/Thermography/MedicalInfraredImaging/forms.py
+++ testCode/pilotProject/Thermography/MedicalInfraredImaging/forms.py
@@ -4,4 +4,19 @@ Author : Michael Xuan
 Project: Study
 Email  : michaelxuan@hotmail.com
 """
-# to-do
+
+from flask_ckeditor import CKEditorField
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, PasswordField, BooleanField, IntegerField, \
+    TextAreaField, SubmitField, MultipleFileField
+from wtforms.validators import DataRequired, Length, ValidationError, Email
+
+
+class PatientForm(FlaskForm):
+    clinicNum = IntegerField("门诊号", validators=[DataRequired()])
+    patientName = StringField("姓名")
+    patientSex = StringField("性别", validators=[Length(1)])
+    patientPhone = IntegerField("电话")
+    patientAddr = StringField("地址")
+    patientID = IntegerField("身份证")
