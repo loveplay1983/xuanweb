a7c2db9202e09d120c2b74a09ac93a608cc9cd85
loveplay1983 <michaelxuan@hotmail.com>
2022-09-04 08:44:15 +0800

    office update

--- testCode/pilotProject/Thermography/MedicalInfraredImaging/static/js/script.js
+++ testCode/pilotProject/Thermography/MedicalInfraredImaging/static/js/script.js
@@ -1,4 +1,4 @@
-/* Datetime display in the navbar */
+/***************************************  Datetime display in the navbar *********************************/
 
 // Function to format 1 in 01
 const zeroFill = n => {
@@ -14,44 +14,22 @@ const interval = setInterval(() => {
         zeroFill(now.getMinutes()) + ':' +
         zeroFill(now.getSeconds());
 }, 1000);
+/***************************************  Datetime display in the navbar *********************************/
 
 
-/* Two tabs of the main content */
-function openPage(pageName, elmnt, color) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
-    document.getElementById(pageName).style.display = "block";
-    elmnt.style.backgroundColor = color;
-}
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
-/* Query data without by only pressing the Return key */
+/*************************************** Query data by pressing the Return Key *********************************/
 
 // Retrieve Patient info
 document.getElementById("patientNum")
-    .addEventListener('keyup', function(event) {
-        if (event.code === 'Enter')
-        {
+    .addEventListener('keyup', function (event) {
+        if (event.code === 'Enter') {
             event.preventDefault();
+            alert("It worked!")
             // document.querySelector("form").submit();
+            document.querySelectorAll("form")[0].reset();
             document.querySelectorAll("form")[0].submit();
             // document.getElementById("patientNum").submit();
-            // alert("It worked!")
+
         }
     });
-
-
-// test for retrieving patient data
-function test(){
-    document.getElementById("collect_patient_form").reset();
-}
-
+/*************************************** Query data by pressing the Return Key *********************************/
