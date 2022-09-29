27fb8ab99adc5e4a2256d203eb6e4d24bf59b3b4
loveplay1983 <michaelxuan@hotmail.com>
2022-09-29 13:19:57 +0800

    office update

--- CodeTest/pilotProject/Thermography/MedicalInfraredImaging/static/js/script.js
+++ CodeTest/pilotProject/Thermography/MedicalInfraredImaging/static/js/script.js
@@ -50,3 +50,53 @@ function clinicBtn(){
 
 }
 
+
+// patient image slide show
+var slideIndex = 1;
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+
+    // Reset the index if the current index exceeds the min and max
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    // Initialize all the slides to none object
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    // Show up the image with the chosen index
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += "active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+showSlides(slideIndex);
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
