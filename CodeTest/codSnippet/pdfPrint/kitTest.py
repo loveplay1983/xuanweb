ccea78c7a969225df0c023cd0b334b255f50287f
loveplay1983 <michaelxuan@hotmail.com>
2022-10-17 16:29:42 +0800

    office update

--- CodeTest/codSnippet/pdfPrint/kitTest.py
+++ CodeTest/codSnippet/pdfPrint/kitTest.py
@@ -5,9 +5,20 @@ Project: Study
 Email  : michaelxuan@hotmail.com
 """
 
-# import pdfkit
-# pdfkit.from_url("http://localhost:5000/patients/10", "10.pdf")
+import pdfkit
 
+options = {
+    # 'quiet': '',
+    'page-size': 'A4',
+    'margin-top': '0.0001cm',
+    'margin-right': '0.0001cm',
+    'margin-bottom': '0.0001cm',
+    'margin-left': '0.0001cm',
+    'disable-smart-shrinking': '',
+    'dpi': 300
+}
 
-from weasyprint import HTML
-pdf = HTML("http://localhost:5000/patients/10").write_pdf("weasy.pdf")
+pdfkit.from_url("http://localhost:5000/patients/4", "4.pdf", options=options)
+
+# from weasyprint import HTML
+# pdf = HTML("http://localhost:5000/patients/10").write_pdf("weasy.pdf")
