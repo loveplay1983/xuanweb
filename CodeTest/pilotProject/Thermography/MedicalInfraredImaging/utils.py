00695c927a7133c0ce36bcdda5a5d1cbca4ec8f6
loveplay1983 <michaelxuan@hotmail.com>
2022-10-19 10:52:12 +0800

    office update

--- CodeTest/pilotProject/Thermography/MedicalInfraredImaging/utils.py
+++ CodeTest/pilotProject/Thermography/MedicalInfraredImaging/utils.py
@@ -8,6 +8,7 @@ from MedicalInfraredImaging import app
 import os
 import re
 import uuid
+import secrets
 import json
 import datetime
 
@@ -43,3 +44,13 @@ class MyEncoder(json.JSONEncoder):
 def removeTag(raw):
     cleaner = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
     return re.sub(cleaner, "", raw)
+
+
+def genSecretKey():
+    # return os.urandom(12)
+    # return secrets.token_urlsafe(16)
+    # return secrets.token_bytes(12)
+    # return secrets.token_hex(12)
+    randKey = uuid.uuid4().hex
+    with open(".env", "w") as env:
+        env.write(f"SECRET_KEY={randKey}")
